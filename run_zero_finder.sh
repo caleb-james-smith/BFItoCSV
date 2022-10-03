1ed75beb29a34e4fc5891057f9688c7812870047
Caleb James Smith <colorado.caleb.smith@gmail.com>
2022-10-03 13:06:10 -0500

    Comment out pandas set_option commands to fix error.

--- run_zero_finder.sh
+++ run_zero_finder.sh
@@ -4,8 +4,8 @@
 #output prints to screen so pipe into a text file
 
 CSVDIR=BFI_NanoAODv9_T4bd_allbkg_2022_09_28_v1
-python zero_finder.py ${CSVDIR} > zeros_${CSVDIR}.log
+python3 zero_finder.py ${CSVDIR} > zeros_${CSVDIR}.log
 
 CSVDIR=BFI_NanoAODv9_T4bd_ttbar_2022_09_28_v1
-python zero_finder.py ${CSVDIR} > zeros_${CSVDIR}.log
+python3 zero_finder.py ${CSVDIR} > zeros_${CSVDIR}.log
 
